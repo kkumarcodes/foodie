0d948618ef575d956593698f3085bd93cbf1bf76
Kyle Kumar <62655757+kkumarcodes@users.noreply.github.com>
2023-07-12 09:15:23 -0400

    Add files via upload

---
+++ quasar.conf.js
@@ -0,0 +1,112 @@
+/*
+ * This file runs in a Node context (it's NOT transpiled by Babel), so use only
+ * the ES6 features that are supported by your Node version. https://node.green/
+ */
+
+// Configuration for your app
+// https://quasar.dev/quasar-cli/quasar-conf-js
+
+/* eslint-env node */
+const ESLintPlugin = require('eslint-webpack-plugin');
+const { configure } = require('quasar/wrappers');
+const path = require('path');
+
+module.exports = configure(function (ctx) {
+  return {
+    // https://quasar.dev/quasar-cli/supporting-ts
+    supportTS: false,
+
+    // https://quasar.dev/quasar-cli/prefetch-feature
+    // preFetch: true,
+
+    // app boot file (/src/boot)
+    // --> boot files are part of "main.js"
+    // https://quasar.dev/quasar-cli/boot-files
+    boot: ['config', 'firebase', 'axios'],
+
+    // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
+    css: ['app.scss'],
+
+    // https://github.com/quasarframework/quasar/tree/dev/extras
+    extras: [
+      // 'ionicons-v4',
+      // 'mdi-v5',
+      // 'fontawesome-v5',
+      'eva-icons',
+      // 'themify',
+      // 'line-awesome',
+      // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
+
+      // 'roboto-font', // optional, you are not bound to it
+      'material-icons', // optional, you are not bound to it
+      'material-icons-outlined'
+    ],
+
+    // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
+    build: {
+      vueRouterMode: 'history', // available values: 'hash', 'history'
+      env: require('dotenv').config({ debug: true }).parsed,
+
+      // transpile: false,
+      // publicPath: '/',
+
+      // Add dependencies for transpiling with Babel (Array of string/regex)
+      // (from node_modules, which are by default not transpiled).
+      // Applies only if "transpile" is set to true.
+      // transpileDependencies: [],
+
+      // rtl: true, // https://quasar.dev/options/rtl-support
+      // preloadChunks: true,
+      // showProgress: false,
+      // gzip: true,
+      // analyze: true,
+
+      // Options below are automatically set depending on the env, set them if you want to override
+      // extractCSS: false,
+
+      // https://quasar.dev/quasar-cli/handling-webpack
+      // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
+      chainWebpack(chain) {
+        chain
+          .plugin('eslint-webpack-plugin')
+          .use(ESLintPlugin, [{ extensions: ['js', 'vue'] }]);
+      },
+      extendWebpack(cfg) {
+        cfg.resolve.alias = {
+          ...cfg.resolve.alias,
+          '@utils': path.resolve(__dirname, './src/utils'),
+          '@stores': path.resolve(__dirname, './src/stores')
+        };
+      }
+    },
+
+    // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
+    devServer: {
+      server: {
+        type: 'http'
+      },
+      port: 8080,
+      open: true // opens browser window automatically
+    },
+
+    // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
+    framework: {
+      config: {
+        dark: false // or Boolean true/false
+      },
+
+      // Quasar plugins
+      plugins: ['Loading', 'Dialog', 'Notify']
+    },
+
+    // Full list of options: https://quasar.dev/quasar-cli/developing-capacitor-apps/configuring-capacitor
+    capacitor: {
+      hideSplashscreen: false
+    },
+
+    bin: {
+      linuxAndroidStudio:
+        '/snap/android-studio/current/android-studio/bin/studio.sh'
+    }
+  };
+});
