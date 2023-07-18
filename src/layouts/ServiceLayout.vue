357feccbdc15bb1b90339c6379912c6c0b0fde0d
Kyle Kumar <62655757+kkumarcodes@users.noreply.github.com>
2023-07-18 04:16:48 -0400

    Create ServiceLayout.vue

---
+++ src/layouts/ServiceLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-layout view="hHh lpr fFf">
+    <q-header>
+      <AppHeader />
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+  import AppHeader from 'components/common/navigation/AppHeader.vue';
+</script>
