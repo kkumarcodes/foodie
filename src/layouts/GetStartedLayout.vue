9dbdb0a46188bb9a7fdcd1bf952b7125c7a95a37
Kyle Kumar <62655757+kkumarcodes@users.noreply.github.com>
2023-07-18 04:16:19 -0400

    Create GetStartedLayout.vue

---
+++ src/layouts/GetStartedLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="hHh lpr fff" class="bg-white text-white">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+  import { defineComponent, onBeforeMount } from 'vue';
+  import { setStatusBarColor } from '@utils/helpers/common';
+
+  export default defineComponent({
+    name: 'GetStartedLayout',
+    components: { },
+    setup() {
+      onBeforeMount(() => {
+        setStatusBarColor('primary-700');
+      });
+      return {};
+    }
+  });
+</script>
